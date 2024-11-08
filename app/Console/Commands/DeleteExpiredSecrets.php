12fef71a3826e30cdce39c9501c3c0e1282d93a1
Soufiane Achaoui <soufiane.achaoui@indicia.nl>
2024-11-08 15:49:20 +0100

    Created command to delete expired secrets

---
+++ app/Console/Commands/DeleteExpiredSecrets.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Secret;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class DeleteExpiredSecrets extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-expired-secrets';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes all expired secrets from the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $secretsDeleted = Secret::where('expires_at', '<', Carbon::now('UTC'))->delete();
+        Log::info('Deleted ' . $secretsDeleted . ' secrets.');
+    }
+}
